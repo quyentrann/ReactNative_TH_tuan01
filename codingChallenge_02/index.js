c8cc9e9b5c4da378316aada13447100cc42d23dc
H51M14\Student <Student@gmail.com>
2024-08-21 08:06:37 +0700

    react exercises codingChallenge3

--- codingChallenge_02/index.js
+++ codingChallenge_02/index.js
@@ -1,11 +1,21 @@
+// var Mark = {
+//     mass: 95,
+//     height: 1.88
+// }
+// var john = {
+//     mass: 85,
+//     height: 1.76
+// }
+
 var Mark = {
-    mass: 95,
-    height: 1.88
+    mass: 78,
+    height: 1.69
 }
 var john = {
-    mass: 85,
-    height: 1.76
+    mass: 92,
+    height: 1.95
 }
+
 var BMI_Mark = Mark.mass / (Mark.height ** 2)
 var BMI_John = john.mass / (john.height ** 2)
 
